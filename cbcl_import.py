324529afef607b8ba245a5d80807bd7b997f1fb0
Haley Acevedo <haley.acevedo2@gmail.com>
2018-08-22 16:02:28 -0500

    removed need for redundant arguments (most vars now sourced from data dict)

--- cbcl_import.py
+++ cbcl_import.py
@@ -2,14 +2,22 @@ import argparse
 import numpy as np
 import pandas as pd
 import re
-import sys
-
-sys.path.append(r'H:\REDCap Scripts')
 
 from collections import OrderedDict
 from gooey import Gooey, GooeyParser
 from os.path import join
-from redcap_common import expand
+
+# NOTE: do not try to expand the df for non-longitudal projects - it screws up the column order which this code heavily relies on
+# from redcap_common import expand
+
+var_search_map = {
+    'Number of favorite sports listed': 'num_sports',
+    'any academic or other problems in school': 'acad_prob',
+    '1. Acts too young for his/her age': 'q1',
+    '58. Picks nose, skin, or other parts of body': 'q58',
+    '59. Plays with own sex parts in public': 'q59',
+    '113': 'q113'
+}
 
 def card1_specific_changes(card1):
     incorrectly_numbered = [ col for col in card1.axes[0].tolist() if col.startswith('cbcl_number') or col == 'cbcl_times_a_week_friends' ]
@@ -46,27 +54,27 @@ def get_matching_col(row, pattern):
     return [ col for col in row.axes[0].tolist() if re.match(pattern, col) ][0]
 
 
-def gen_subject_file(row, outfolder, question_prefix, card1_bounds, demo_cols):
-    study_id = row.name
+def gen_subject_file(row, outfolder, var_map):
+    study_id = row.name + '_'
+    study_id += row['redcap_event_name'].replace('_', '') if 'redcap_event_name' in row.axes[0] else var_map['q1'].split('_')[0]
     print('Processing ', study_id)
 
-    if row[list(demo_cols.values())].isnull().any():
+    if row[[var_map['gender'], var_map['age']]].isnull().any():
         print('Skipping subject {} due to missing age and/or gender'.format(study_id))
         return
 
-    study_id = '_'.join([study_id, row['session_number'].replace('_', '')])
-    subj_str = '{: >10}{}{}{:02}'.format(study_id[:10], '{}', int(row[demo_cols['gender']]), int(row[demo_cols['age']]))
+    subj_str = '{: >10}{}{}{:02}'.format(study_id[:10], '{}', int(row[var_map['gender']]), int(row[var_map['age']]))
 
     cards = OrderedDict([
-        ('01', card1_bounds),
-        ('02', (question_prefix + '1',  question_prefix + '58')),
-        ('03', (question_prefix + '59', get_matching_col(row, question_prefix + '113')))
+        ('01', (var_map['num_sports'] if 'num_sports' in var_map else None, var_map['acad_prob'] if 'acad_prob' in var_map else None)),
+        ('02', (var_map['q1'], var_map['q58'])),
+        ('03', (var_map['q59'], var_map['q113']))
     ])
 
     line = ''
     for num, col_tuple in cards.items():
         line += subj_str.format(num)
-        if num == '01' and not all(card1_bounds):
+        if num == '01' and not all(col_tuple):
             line += ' ' * 18 + '9' * 38
         else:
             line += card(num, row, col_tuple)
@@ -82,27 +90,42 @@ def gen_subject_file(row, outfolder, question_prefix, card1_bounds, demo_cols):
     return
 
 
-def cbcl_import(file, data_dict, outfolder, question_prefix, card1_bounds=(None,None)):
+def cbcl_import(file, data_dict, outfolder, age_col):
     df = pd.read_csv(file, index_col=0)
 
     data_dict_df = pd.read_csv(data_dict, index_col=0)
-    cbclq_df = data_dict_df[data_dict_df['Field Type'] == 'radio']
-    keep_qcols = [ col for col in list(cbclq_df.index.values) if col.startswith(question_prefix) and col in df.columns ]
-    df.drop(columns=[ col for col in df.columns if col.startswith(question_prefix) and col not in keep_qcols], inplace=True)
 
-    # if non-longitudinal, expand the df; otherwise, rename the event column to match expanded naming scheme
-    df = redcap_common.expand(df) if 'redcap_event_name' not in df.columns else df.rename(columns={'redcap_event_name': 'session_number'})
+    # construct map of card bound variables
+    #   var_map example: { 'q1': [ 's1s1_cbcl_q1', 's2_cbcl_q2'], 'q58': ['s1_cbcl_q58', 's2_cbcl_q58'] }
+    var_map = { v: data_dict_df.index[(data_dict_df['Field Label'].str.contains(k, na=False)) & (data_dict_df['Field Type'] == 'radio')].tolist() for k,v in var_search_map.items() }
+    var_map = { k: v for k,v in var_map.items() if v }
 
     gender_cols = [ col for col in df.columns if 'gender' in col ]
-    age_cols = [ col for col in df.columns if 'age' in col ]
+    age_cols = [ col for col in df.columns if re.search(age_col, col) ]
     if len(gender_cols) < 1 or len(age_cols) < 1:
         sys.stderr.write('Age and gender columns must be included in REDCap export.')
         sys.exit(2)
-    demo_cols = { 'gender': gender_cols[0], 'age': age_cols[0] }
 
-    df = df[df[keep_qcols].sum(axis=1) != 0] # get rid of empty rows (all nan or 0)
-    print(df)
-    df.apply(gen_subject_file, args=(outfolder, question_prefix, card1_bounds, demo_cols), axis=1)
+    # convert var_map to be a list of dicts (where each dict has same set of keys, but each with one of the lists values)
+    #   needed to handle non-longitudal REDCap structure which will have multiple matching variables
+    #   num_var_list structure: [ { 'q1': 's1_cbcl_q1', 'q58': 's1_cbcl_q58' }, {'q1': 's2_cbcl_q1', 'q58': 's2_cbcl_q58' } ]
+    num_var_lists = len(var_map['q1'])
+    var_map_list = [ {} for i in range(num_var_lists) ]
+    for k,v in var_map.items():
+        for i in range(num_var_lists):
+            var_map_list[i][k] = v[i]
+            var_map_list[i]['gender'] = gender_cols[0]
+            var_map_list[i]['age'] = age_cols[i]
+
+    for i, map in enumerate(var_map_list):
+        print(map)
+        start_q = map['num_sports'] if 'num_sports' in map else map['q1']
+        cbclq_df = data_dict_df[start_q:map['q113']]
+        keep_qcols = cbclq_df[cbclq_df['Field Type'] == 'radio'].index.tolist()
+        df.drop(columns=[ col for col in cbclq_df.index.tolist() if col in df and col not in keep_qcols], inplace=True)
+
+        df = df[df[keep_qcols].sum(axis=1) != 0] # get rid of empty rows (all nan or 0)
+        df.apply(gen_subject_file, args=(outfolder, map), axis=1)
 
 
 @Gooey()
@@ -113,21 +136,11 @@ def parse_args():
     required_group.add_argument('--export_file', required=True, widget='FileChooser', help='Demographics + CBCL export from REDCap')
     required_group.add_argument('--data_dict', required=True, widget='FileChooser', help='REDCap data dictionary export for CBCL')
     required_group.add_argument('--output_folder', required=True, widget='DirChooser', help='Folder to store output CBC files')
-    required_group.add_argument('--question_prefix', required=True, help='Common prefix for REDCap variables for the numbered CBCL questions (i.e. cbcl_, s1_cbcl_q)')
-
-    card1_group = parser.add_argument_group('Competence Scale Arguments', 'Leave blank if this section of CBCL was not collected (otherwise, must specify both)')
-    card1_group.add_argument('--num_sports_var', help='REDCap variable for number of sports')
-    card1_group.add_argument('--academic_problems_var', help='REDCap variable for academic/school problems')
-
+    required_group.add_argument('--age_var', required=True, help='REDCap variable containing subject age (i.e. childs_age, age_decimal)')
     return parser
 
 if __name__ == '__main__':
     parser = parse_args()
     args = parser.parse_args()
 
-    card1_bounds = (args.num_sports_var, args.academic_problems_var)
-    if any(card1_bounds) and not all(card1_bounds):
-        parser.error('Both num_sports_var and academic_problems_var need to be provided to identify the sports/activities section of the CBCL')
-        sys.exit(1)
-
-    cbcl_import(args.export_file, args.data_dict, args.output_folder, args.question_prefix, card1_bounds)
+    cbcl_import(args.export_file, args.data_dict, args.output_folder, args.age_var)
