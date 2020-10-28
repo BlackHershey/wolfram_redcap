885a2320adde9114f72f0543691b4a197d693eb9
Jon Koller <kollerj@wustl.edu>
2020-10-28 11:32:18 -0500

    Update med hx dx js code
     - Add the rest of the dx rule types
     - Add the rest of the diagnoses

--- js/medical_history_update.js
+++ js/medical_history_update.js
@@ -1,49 +1,73 @@
-function reformatDxFieldLabel(divName) {
+function reformatDxFieldLabel(dxCodeName) {
+	var divName = dxCodeName + "_dx_label";
+	// var labelDivHTML = "<h6>" + dxName + ": diagnosis</h6><br />";
 	var labelDivHTML = "";
 	var labelDiv = document.getElementById(divName);
 	var dxDateRule = labelDiv.getElementsByClassName("dx_date_rule")[0].childNodes[0].innerHTML;
 	var dxAge = Number(labelDiv.getElementsByClassName("dx_age")[0].childNodes[0].innerHTML);
+	var dxDateNotes = labelDiv.getElementsByClassName("dx_date_notes")[0].childNodes[0].innerHTML;
+	var dxDate = labelDiv.getElementsByClassName("dx_date")[0].childNodes[0].innerHTML;
+	var dxDateEst = labelDiv.getElementsByClassName("dx_date_est")[0].childNodes[0].innerHTML;
+
+	// parse estimated dx date
+	if ( dxDateEst.length > 6 ){
+		var dxDateEstArray = dxDateEst.split("-");
+		var dxYear = dxDateEstArray[0];
+		var dxMonth = Number(dxDateEstArray[1]);
+		dxMonth = dxMonth - 1;
+		var dxDay = dxDateEstArray[2];
+		var dxDateObj = new Date(dxYear, dxMonth, dxDay);
+		dxDay = parseInt(dxDay).toString();
+		var dxMonthLong = dxDateObj.toLocaleString('default', { month: 'long' });
+	} else if ( dxDate.length > 6 ){
+		var dxDateArray = dxDate.split("-");
+		var dxYear = dxDateArray[0];
+		var dxMonth = Number(dxDateArray[1]);
+		dxMonth = dxMonth - 1;
+		var dxDay = dxDateArray[2];
+		var dxDateObj = new Date(dxYear, dxMonth, dxDay);
+		dxDay = parseInt(dxDay).toString();
+		var dxMonthLong = dxDateObj.toLocaleString('default', { month: 'long' });
+	}
+
+	// dx date/age
+	labelDivHTML += "<span style=\"color:#0e1f8a;\">Diagnosis:</span> "
 	// test dx date rule
     switch(dxDateRule) {
     	case "Date":
-			var dxDate = labelDiv.getElementsByClassName("dx_date")[0].childNodes[0].innerHTML;
-    		labelDivHTML = "The actual dx date is " + dxDate + ", age " + dxAge.toFixed(2) + ".";
+    		labelDivHTML += "The <i>actual dx date</i> is " + dxMonthLong + " " + dxDay + ", " + dxYear + "; age = " + dxAge.toFixed(2) + ".";
             break;
   		case "Age":
-    		labelDivHTML = "Estimated dx at age " + dxAge.toFixed(1);
+    		labelDivHTML += "<i>Estimated age</i> at dx = " + dxAge.toFixed(1);
 			break;
 		case "Month":
-			var dxDateEst = labelDiv.getElementsByClassName("dx_date_est")[0].childNodes[0].innerHTML;
-			var dxDateEstArray = dxDateEst.split("-");
-			var dxYear = dxDateEstArray[0];
-			var dxMonth = Number(dxDateEstArray[1]);
-			dxMonth = dxMonth - 1;
-			var dxDay = dxDateEstArray[2];
-			var dxDateObj = new Date(dxYear, dxMonth, dxDay);
-			var dxMonthLong = dxDateObj.toLocaleString('default', { month: 'long' });
-			labelDivHTML = "Estimated dx at " + dxMonthLong + " " + dxYear + ", age " + dxAge.toFixed(2);
+			labelDivHTML += "<i>Estimated</i> dx at <i>" + dxMonthLong + " " + dxYear + "</i>; age = " + dxAge.toFixed(2);
+			break;
+		case "Year":
+			labelDivHTML += "<i>Estimated</i> dx in <i>year " + dxYear + "</i> (used " + dxMonthLong + " " + dxDay + ", " + dxYear + "); age = " + dxAge.toFixed(2);
 			break;
 		case "Clinic dx":
-			var dxDateEst = labelDiv.getElementsByClassName("dx_date_est")[0].childNodes[0].innerHTML;
-			var dxDateEstArray = dxDateEst.split("-");
-			var dxYear = dxDateEstArray[0];
-			var dxMonth = Number(dxDateEstArray[1]);
-			dxMonth = dxMonth - 1;
-			var dxDay = dxDateEstArray[2];
-			var dxDateObj = new Date(dxYear, dxMonth, dxDay);
-			var dxMonthLong = dxDateObj.toLocaleString('default', { month: 'long' });
-			labelDivHTML = "Clinic dx at the " + dxYear + " clinic (" + dxMonthLong + " " + dxYear + "), age " + dxAge.toFixed(2);
+			labelDivHTML += "<i>Clinic dx</i> at the " + dxYear + " clinic (" + dxMonthLong + " " + dxYear + "); age = " + dxAge.toFixed(2);
 			break;
 		case "Dx age unknown":
-			labelDivHTML = "Diagnosis present, but age unknown";
+			labelDivHTML += "Diagnosis present, but age unknown";
 			break;
 		case "No dx":
-			labelDivHTML = "No diagnosis";
+			labelDivHTML += "No diagnosis";
 			break;
   		default:
     		// code block
 	}
+	labelDivHTML += "<br /><br />";
+
+	// dx date/age notes
+	labelDivHTML += "<span style=\"color:#0e1f8a;\">Notes:</span> "
+	labelDivHTML += dxDateNotes;
     labelDiv.innerHTML = labelDivHTML;
 }
-window.addEventListener('load', function(){reformatDxFieldLabel("wfs_dx_label")})
-window.addEventListener('load', function(){reformatDxFieldLabel("hearloss_dx_label")})
+window.addEventListener('load', function(){reformatDxFieldLabel("wfs")})
+window.addEventListener('load', function(){reformatDxFieldLabel("dm")})
+window.addEventListener('load', function(){reformatDxFieldLabel("di")})
+window.addEventListener('load', function(){reformatDxFieldLabel("hearloss")})
+window.addEventListener('load', function(){reformatDxFieldLabel("oa")})
+window.addEventListener('load', function(){reformatDxFieldLabel("bladder")})
